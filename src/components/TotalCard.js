a04e25db2a4031af54455f7da3f83e6c6c6a1db0
Andon Dimov <a_dimov@abv.bg>
2023-03-16 21:09:06 +0100

    Total bachground change..

--- src/components/TotalCard.js
+++ src/components/TotalCard.js
@@ -2,26 +2,32 @@ import React from "react";
 import { useBudgets } from "../contexts/BudgetContext";
 
 const TotalCard = () => {
-  const {budgets, expenses} = useBudgets();
-  const totalBudgets = budgets.reduce((total, budget) =>{
-    return (total=total+budget.max);
-  },0);
-  const totalExpenses = expenses.reduce((total, expense) =>{
-    return (total=total+expense.amount);
-  },0);
-
+  const { budgets, expenses } = useBudgets();
+  const totalBudgets = budgets.reduce((total, budget) => {
+    return (total = total + budget.max);
+  }, 0);
+  const totalExpenses = expenses.reduce((total, expense) => {
+    return (total = total + expense.amount);
+  }, 0);
+  const cardBg = (expenses, total) => {
+    const ratio = expenses / total;
+    if (ratio < 0.5) return "bg-info";
+    if (ratio < 0.75) return "bg-warning";
+    return "bg-danger";
+  };
+  
   return (
-    <div className="card mt-3 bg-info">
-    <div className="card-body">
-      <h3 className="card-title hstack align-items-baseline fw-normal">
-        <div>Total</div>
-        <div className="ms-auto fs-3">
-          {totalExpenses}
-          <span className="text-muted ms-1">/ {totalBudgets}</span>
-        </div>
-      </h3>
+    <div className={"card mt-3 "+cardBg(totalExpenses,totalBudgets)}>
+      <div className="card-body">
+        <h3 className="card-title hstack align-items-baseline fw-normal">
+          <div>Total</div>
+          <div className="ms-auto fs-3">
+            {totalExpenses}
+            <span className="text-muted ms-1">/ {totalBudgets}</span>
+          </div>
+        </h3>
+      </div>
     </div>
-  </div>
   );
 };
 
