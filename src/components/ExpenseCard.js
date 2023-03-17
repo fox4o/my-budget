12ecf3eaef821bfb21034e062f8b5f019b5cd6a6
Andon Dimov <a_dimov@abv.bg>
2023-03-17 21:51:19 +0100

    Interface improwed

--- src/components/ExpenseCard.js
+++ src/components/ExpenseCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useBudgets } from "../contexts/BudgetContext";
+import { currency } from "../utils";
 import ExpenseList from "./ExpenseList";
 
 const ExpanseCard = ({ id, name, max }) => {
-  const { setBudgetId, getBudgetExpenses, deleteBudget } =
-    useBudgets();
+  const { setBudgetId, getBudgetExpenses, deleteBudget } = useBudgets();
   const expenses = getBudgetExpenses(id);
   const amount = expenses.reduce((total, expanse) => total + expanse.amount, 0);
 
@@ -12,11 +12,11 @@ const ExpanseCard = ({ id, name, max }) => {
     <>
       <div className="card mt-3">
         <div className="card-body">
-          <h5 className="card-title hstack align-items-baseline fw-normal">
-            <div className="me-auto">{name}</div>
-            <div className="d-flex align-items-baseline mx-auto">
-              {amount}
-              <span className="text-muted ms-1">/ {max}</span>
+          <h4 className="card-title hstack align-items-baseline fw-normal">
+            <div className="me-auto">{name.slice(0, 8)}</div>
+            <div className="d-flex align-items-baseline pe-1 text-right fs-6">
+              {currency.format(amount)}
+              <span className="text-muted ms-1">/ {currency.format(max)}</span>
             </div>
             {expenses.length > 0 ? (
               <button
@@ -44,7 +44,7 @@ const ExpanseCard = ({ id, name, max }) => {
             >
               <i className="bi bi-file-earmark-plus"></i>
             </button>
-          </h5>
+          </h4>
           <div className="progress rounded-pill">
             <div
               className={"progress-bar " + setProgressBar(amount, max)}
